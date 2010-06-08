18b71fa6f169e75dcbced1a8a91b540e5660fa0c
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-06-08 22:10:08 +0900

    Plot the equation of time on a graph with dates along the x-axis.

--- equation_of_time.py
+++ equation_of_time.py
@@ -19,12 +19,28 @@ def main():
     #matplotlib.use('pdf')
     #matplotlib.use('svg')
     from matplotlib import pyplot as plt
+    import datetime
 
 
     day_numbers = np.arange(0, 365.242, 0.01)
-    plt.plot(day_numbers, equation_of_time(day_numbers))
+#    plt.plot(day_numbers, equation_of_time(day_numbers))
+    date_range = day_numbers + matplotlib.dates.date2num(datetime.date(2000,1,1))
+    plt.plot_date(date_range, equation_of_time(day_numbers), '-')
 
-    plt.axis('tight')
+#    plt.xlabel('day number')
+#    plt.xlim([min(day_numbers), max(day_numbers)])
+    rule = matplotlib.dates.rrulewrapper(matplotlib.dates.MONTHLY)
+    loc = matplotlib.dates.RRuleLocator(rule)
+    formatter = matplotlib.dates.DateFormatter('%b')
+    ax = plt.subplot(111)
+    ax.xaxis.set_major_locator(loc)
+    ax.xaxis.set_major_formatter(formatter)
+    labels = ax.get_xticklabels()
+    plt.setp(labels, rotation=30, fontsize=10)
+
+    plt.ylabel('solar time vs clock time (min)')
+    plt.grid(True)
+
     plt.show()
 #    plt.savefig('horiz.pdf')
 #    plt.savefig('horiz.svg')

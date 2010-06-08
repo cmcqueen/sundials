548f8f5908d8682803d1c27fdb7bf1b78b5e537f
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-06-08 20:06:25 +0900

    Change handling of southern hemisphere calculations. Add compass arrow.

--- horiz.py
+++ horiz.py
@@ -25,7 +25,7 @@ HOUR_LINE_MAX = 19
 GNOMON_LENGTH = 0.9
 NUMERAL_OFFSET = 1.07
 EXTENT_MAJOR = 1.15
-EXTENT_MINOR = 0.65
+EXTENT_MINOR = 0.7
 
 
 def equatorial_hour_angle(hour, location, timezone):
@@ -45,49 +45,57 @@ def horiz_hour_angle(hour, location, timezone):
     # negative (am) is towards the west; positive (pm) towards the east
     a_x = np.sin(equatorial_angle_from_solar_noon)
     a_y = np.cos(equatorial_angle_from_solar_noon)
-    horiz_angle_from_solar_noon = np.arctan2(np.sin(np.deg2rad(location.latitude)) * a_x, a_y)
+    horiz_angle_from_solar_noon = np.arctan2(a_x, a_y / np.sin(np.deg2rad(location.latitude)))
     logging.debug("For hour %d, horiz angle from solar noon %g" % (hour, np.rad2deg(horiz_angle_from_solar_noon)))
 
-    if location.latitude >= 0:
-        # sun is in the south; shadows fall towards the north
-        return np.pi/2 - horiz_angle_from_solar_noon
-    else:
-        # sun is in the north; shadows fall towards the south
-        return -np.pi/2 + horiz_angle_from_solar_noon
+    # Angle currently is angle referenced from solar noon, positive (pm) towards the east.
+    # Change to mathematical angle, anticlockwise from 0 in the east.
+    return np.pi / 2 - horiz_angle_from_solar_noon
 
 
 def main():
     fig = plt.figure()
     ax1 = fig.add_subplot(111, aspect='equal')
-    
+
     for hour in range(HOUR_LINE_MIN, HOUR_LINE_MAX + 1):
         horiz_angle = horiz_hour_angle(hour, LOCATION, TIMEZONE)
+        if LOCATION.latitude < 0:
+            # For southern hemisphere, rotate the whole thing around by 180
+            # degrees, so "up" is consistently from the sundial viewer's
+            # perspective with the sun behind their shoulder.
+            horiz_angle += np.deg2rad(180)
         logging.info("For hour %d, horiz angle %g" % (hour, np.rad2deg(horiz_angle)))
         line = lines.Line2D([0, np.cos(horiz_angle)], [0, np.sin(horiz_angle)])
         ax1.add_line(line)
         hour_text = "%d" % ((hour - 1) % 12 + 1)
         ax1.add_artist(text.Text(np.cos(horiz_angle) * NUMERAL_OFFSET, np.sin(horiz_angle) * NUMERAL_OFFSET, hour_text, ha='center', va='center'))
-    
+
     # Draw the position for the gnomon
-    if LOCATION.latitude >= 0:
-        gnomon_extent_y = GNOMON_LENGTH
-    else:
-        gnomon_extent_y = -GNOMON_LENGTH
-    gnomon_line = lines.Line2D([0, 0], [0, gnomon_extent_y], color='red')
+    gnomon_line = lines.Line2D([0, 0], [0, GNOMON_LENGTH], color='red')
     ax1.add_line(gnomon_line)
-    
+
+    # Draw a compass arrow
+    if LOCATION.latitude >= 0:
+        # Up for northern hemisphere
+        ax1.add_artist(text.Text(0, -0.25, "N", ha='center', va='center'))
+        arrow = matplotlib.patches.Arrow(0, -0.6, 0, 0.3, width=0.1, edgecolor='none')
+        ax1.add_patch(arrow)
+    else:
+        # Down for the southern hemisphere
+        ax1.add_artist(text.Text(0, -0.6, "N", ha='center', va='center'))
+        arrow = matplotlib.patches.Arrow(0, -0.25, 0, -0.3, width=0.1, edgecolor='none')
+        ax1.add_patch(arrow)
+
     #plt.axis('tight')
     plt.axis('off')
     
     plt.xlim(-EXTENT_MAJOR, EXTENT_MAJOR)
-    if LOCATION.latitude >= 0:
-        plt.ylim(-EXTENT_MINOR, EXTENT_MAJOR)
-    else:
-        plt.ylim(-EXTENT_MAJOR, EXTENT_MINOR)
-    
+    plt.ylim(-EXTENT_MINOR, EXTENT_MAJOR)
+
     plt.show()
 #    plt.savefig('horiz.pdf')
 #    plt.savefig('horiz.svg')
+#    plt.savefig('horiz.png')
 
 
 main()

8ed3b7165a48f9f42235b96fae72eeca2f221119
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-06-20 09:56:38 +0900

    Replace hour lines with points on the ellipse.
    --HG--
    branch : analemmatic

--- analemmatic.py
+++ analemmatic.py
@@ -25,7 +25,7 @@ HOUR_LINE_MIN = 5
 HOUR_LINE_MAX = 19
 EXTENT_MAJOR = 1.2
 EXTENT_MINOR = 0.9
-NUMERAL_OFFSET = 1.15
+NUMERAL_OFFSET = 1.1
 
 def equatorial_hour_angle(hour, location, timezone):
     """Midnight is angle 0.
@@ -50,7 +50,7 @@ def rotated_equatorial_hour_angle(hour, location, timezone):
     return np.pi / 2 - equatorial_angle_from_solar_noon
 
 
-def analemmic_horiz_hour_angle(hour, location, timezone):
+def analemmatic_horiz_hour_angle(hour, location, timezone):
     equatorial_angle = equatorial_hour_angle(hour, location, timezone)
     equatorial_angle_from_solar_noon = equatorial_angle - np.pi
     logging.debug("For hour %d, equatorial angle from solar noon %g" % (hour, equatorial_angle_from_solar_noon * 180 / np.pi))
@@ -63,6 +63,14 @@ def analemmic_horiz_hour_angle(hour, location, timezone):
     # Angle currently is angle referenced from solar noon, positive (pm) towards the east.
     # Change to mathematical angle, anticlockwise from 0 in the east.
     return np.pi / 2 - horiz_angle_from_solar_noon
+
+
+def analemmatic_horiz_hour_position(hour, location, timezone):
+    rotated_equatorial_angle = rotated_equatorial_hour_angle(hour, location, timezone)
+    a_x = np.cos(rotated_equatorial_angle)
+    a_y = np.sin(rotated_equatorial_angle)
+    a_y *= np.sin(np.deg2rad(location.latitude))
+    return (a_x, a_y)
 
 
 def main():
@@ -82,8 +90,6 @@ def main():
                                      width=2,
                                      height=2*np.sin(np.deg2rad(LOCATION.latitude)),
                                      angle=ellipse_rotation,
-#                                     theta1=np.rad2deg(analemmic_angle_min),
-#                                     theta2=np.rad2deg(analemmic_angle_max)
                                      theta1=np.rad2deg(ellipse_angle_max),
                                      theta2=np.rad2deg(ellipse_angle_min),
 #                                     theta1=0,
@@ -91,18 +97,28 @@ def main():
                                      )
     ax1.add_patch(ellipse)
 
+    analemmatic_positions_x = []
+    analemmatic_positions_y = []
     for hour in range(HOUR_LINE_MIN, HOUR_LINE_MAX + 1):
-        analemmic_angle = analemmic_horiz_hour_angle(hour, LOCATION, TIMEZONE)
+        analemmatic_angle = analemmatic_horiz_hour_angle(hour, LOCATION, TIMEZONE)
+        (analemmatic_position_x, analemmatic_position_y) = analemmatic_horiz_hour_position(hour, LOCATION, TIMEZONE)
         if LOCATION.latitude < 0:
             # For southern hemisphere, rotate the whole thing around by 180
             # degrees, so "up" is consistently from the sundial viewer's
             # perspective with the sun behind their shoulder.
-            analemmic_angle += np.deg2rad(180)
-        logging.info("For hour %d, horiz angle %g" % (hour, np.rad2deg(analemmic_angle)))
-        line = lines.Line2D([0, np.cos(analemmic_angle)], [0, np.sin(analemmic_angle)])
-        ax1.add_line(line)
+            analemmatic_angle += np.deg2rad(180)
+            (analemmatic_position_x, analemmatic_position_y) = (-analemmatic_position_x, -analemmatic_position_y)
+        logging.info("For hour %d, horiz angle %g" % (hour, np.rad2deg(analemmatic_angle)))
+        line = lines.Line2D([0, np.cos(analemmatic_angle)], [0, np.sin(analemmatic_angle)])
+#        ax1.add_line(line)
+#        ax1.plot(analemmatic_position_x, analemmatic_position_y, '.')
+        analemmatic_positions_x.append(analemmatic_position_x)
+        analemmatic_positions_y.append(analemmatic_position_y)
         hour_text = "%d" % ((hour - 1) % 12 + 1)
-        ax1.add_artist(text.Text(np.cos(analemmic_angle) * NUMERAL_OFFSET, np.sin(analemmic_angle) * NUMERAL_OFFSET, hour_text, ha='center', va='center'))
+#        ax1.add_artist(text.Text(np.cos(analemmatic_angle) * NUMERAL_OFFSET, np.sin(analemmatic_angle) * NUMERAL_OFFSET, hour_text, ha='center', va='center'))
+        ax1.add_artist(text.Text(analemmatic_position_x * NUMERAL_OFFSET, analemmatic_position_y * NUMERAL_OFFSET, hour_text, ha='center', va='center'))
+
+    ax1.plot(analemmatic_positions_x, analemmatic_positions_y, '.')
 
     # Draw a compass arrow
     if LOCATION.latitude >= 0:

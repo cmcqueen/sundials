7cdb6b8b8bc6f73e32195a28344d2af1c7d5d8a7
Craig McQueen <craig@mcqueen.id.au>
2019-12-29 16:49:07 +1100

    Unix shebang: Change to use Python 3

--- analemmatic.py
+++ analemmatic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Calculation and generation of analemmatic sundial.
 
--- sun_declination.py
+++ sun_declination.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Calculation of sun declination during the year.
 
